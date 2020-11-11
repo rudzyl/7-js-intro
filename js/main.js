1d12c0095fb5cc3d788e2382368a44cd7195d2bb
rudzyl <73877213+rudzyl@users.noreply.github.com>
2020-11-11 13:58:21 +0200

    javascript begins

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// spausdiname sveikaji skaiciu
+console.log(34567890);
+
+// du skaiciai vs desimtainis - kaip veikia ir koks yra kablelis 
+console.log(4,67);
+console.log(4.35);
+
+// kaip spausdinamas testas?
+console.log("labas rytas");
+console.log('Rugile');
+
+
+
